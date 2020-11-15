70e84569abeb329b92d55f074c12ce4ded8ecd18
zackper <zackper1999@gmail.com>
2020-11-15 20:51:58 +0200

    * Merge branch Cretanian DONE

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
-import { TestListComponent } from './test-list/test-list.component';
 import { MatListModule} from '@angular/material/list';
 import { GButtonComponent } from './g-button/g-button.component';
 import { MobileFooterComponent } from './mobile-footer/mobile-footer.component';
@@ -102,7 +101,6 @@ import { FCallSettingsComponent } from './f-call-settings/f-call-settings.compon
 @NgModule({
   declarations: [
     AppComponent,
-    TestListComponent,
     GButtonComponent,
     MobileFooterComponent,
     MoreBarComponent,
