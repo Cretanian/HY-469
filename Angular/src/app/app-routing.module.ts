166f750182279f6f23ae7382ad16a8b3e0fcda7f
Cretanian <stelios.stamatakis.sp.9@gmail.com>
2020-11-17 16:44:31 +0200

    Run some rooting test. I have a basic understanding. I think, i made our start. We need to merge and then proceed. I think

--- Angular/src/app/app-routing.module.ts
+++ Angular/src/app/app-routing.module.ts
@@ -3,8 +3,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { MobileChatComponent } from './mobile-chat/mobile-chat.component';
+import { ExplorerChatComponent } from './Final-Cretanian/explorer-chat/explorer-chat.component';
+import { ExplorerTeamsComponent } from './Final-Cretanian/explorer-teams/explorer-teams.component';
 
 const routes: Routes = [
+  { path: 'explorer-mobile-chat', component: ExplorerChatComponent },
+  { path: '', component: ExplorerTeamsComponent },
   { path: 'mobile-chat', component: MobileChatComponent },
   { path: 'main', component: AppComponent}
 ];
@@ -17,4 +21,4 @@ export class AppRoutingModule {
 
 };
 
-export const routingComponents = [AppComponent, MobileChatComponent]; 
+export const routingComponents = [AppComponent, MobileChatComponent,ExplorerChatComponent]; 
