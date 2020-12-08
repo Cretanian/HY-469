8914933c9c354013ccc4a82437f23c1b21d5c847
Cretanian <stelios.stamatakis.sp.9@gmail.com>
2020-12-08 18:14:56 +0200

    added somethings vol2

--- Angular/frontend/src/app/pages/Cretanian/calendar-event/calendar-event.component.ts
+++ Angular/frontend/src/app/pages/Cretanian/calendar-event/calendar-event.component.ts
@@ -9,10 +9,25 @@ export class CalendarEventComponent implements OnInit {
 
   @Input ('event') event: string;
   @Input ('time') time: string;
-
-  constructor() { }
+  color:string;
+  num:number;
+  constructor() { 
+    this.num = this.getRandomInt(100000,999999);
+    this.color = '#' + this.num.toString();   
+    let shand = document.getElementsByClassName('color') as HTMLCollectionOf<HTMLElement>;
+    if (shand.length != 0) {
+      shand[0].style.backgroundColor = this.color;
+    }
+  }
 
   ngOnInit(): void {
+   
+  }
+
+  public getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
   }
 
 }
