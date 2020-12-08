aeaa5ef839612ae8ab3b7db879fe3d4e58900fcc
Antos2 <antosdapro@gmail.com>
2020-12-08 16:12:15 +0200

    Some basic spawn despawn with menu

--- Angular/frontend/src/app/pages/Table/table-menu/table-menu.component.ts
+++ Angular/frontend/src/app/pages/Table/table-menu/table-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-table-menu',
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TableMenuComponent implements OnInit {
 
-  constructor() { }
+  @Input() enableParticipantSpawn: boolean;
+  display:boolean;
+
+  constructor() { 
+    this.display = true;
+    this.enableParticipantSpawn = true;
+  }
 
   ngOnInit(): void {
+    this.enableParticipantSpawn = true;
+  }
+
+  spawnParticipantsButton(){
+      this.display = false;
+      this.enableParticipantSpawn = false;
+  }
+
+  receiveDisplayParticipantEvent(participantsIsDisplayed: boolean){
+    this.display = true;
+    this.enableParticipantSpawn = !this.enableParticipantSpawn;
+  }
+
+  spawnMenu(){
+    this.display = true;
+  }
+
+  despawnMenu(){
+    this.display = false;
   }
 
 }
