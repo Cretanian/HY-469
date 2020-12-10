87d5e43aa49c6e78c66d5b523c4fb64f217fd608
Antos2 <antosdapro@gmail.com>
2020-12-10 15:17:10 +0200

    Carousel works fine! TODO: small refactor to carousel to be prettier in code

--- Angular/frontend/src/app/pages/Table/table-menu/table-menu.component.ts
+++ Angular/frontend/src/app/pages/Table/table-menu/table-menu.component.ts
@@ -11,8 +11,9 @@ export class TableMenuComponent implements OnInit {
   display: boolean;
   muted: boolean;
   iconMuted: string;
-
   @Input() as: string;
+  carouselLeftIndex: number;
+  carousel: any;
 
   constructor() {
     this.muted = false;
@@ -24,6 +25,9 @@ export class TableMenuComponent implements OnInit {
   ngOnInit(): void {
     this.tvGridWindowSpawned = false;
     this.enableParticipantSpawn = true;
+    this.carouselLeftIndex = 0;
+    this.initializeCarousel();
+    this.displayCarousel(3);
   }
 
   //Button Functions
@@ -62,4 +66,56 @@ export class TableMenuComponent implements OnInit {
   receiveDisplayParticipantEvent(participantsIsDisplayed: boolean) {
     this.enableParticipantSpawn = !this.enableParticipantSpawn;
   }
+
+  incrementIndex(index: number) {
+    if (index == 5) index = 0;
+    else index++;
+    return index;
+  }
+  decrementIndex(index: number) {
+    if (index == 0) index = 5;
+    else index--;
+    return index;
+  }
+
+  initializeCarousel() {
+    this.carousel = new Array(6);
+    for (var i = 0; i < this.carousel.length; i++) {
+      this.carousel[i] = carouselNode(false);
+      this.carousel[i].order = i;
+    }
+    for (var i = 0; i < this.carousel.length - 1; i++) {
+      this.carousel[i].next = this.carousel[i + 1];
+    }
+    this.carousel[this.carousel.length - 1].next = this.carousel[0];
+    console.log(this.carousel[0]);
+  }
+
+  displayCarousel(numberToDisplay: number) {
+    for (var i = 0; i < this.carousel.length; i++) {
+      this.carousel[i].display = false;
+    }
+    var j = this.carouselLeftIndex;
+    var runner = this.carousel[j];
+    for (var i = 0; i < numberToDisplay; i++) {
+      runner.display = true;
+      runner.order = i + 1;
+      runner = runner.next;
+    }
+  }
+
+  carouselLeft() {
+    this.carouselLeftIndex = this.decrementIndex(this.carouselLeftIndex);
+    this.displayCarousel(3);
+  }
+  carouselRight() {
+    this.carouselLeftIndex = this.incrementIndex(this.carouselLeftIndex);
+    this.displayCarousel(3);
+  }
+}
+
+function carouselNode(display: boolean) {
+  var node = { display: true, next: Object, order: 0 };
+  node.display = display;
+  return node;
 }
