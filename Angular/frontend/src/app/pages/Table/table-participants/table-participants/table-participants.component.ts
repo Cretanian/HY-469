7eae71d8b8684da731a2e40842567ae9326d2ea5
Antos2 <antosdapro@gmail.com>
2020-12-13 20:39:09 +0200

    Mute DONE for participants in mobile & table

--- Angular/frontend/src/app/pages/Table/table-participants/table-participants/table-participants.component.ts
+++ Angular/frontend/src/app/pages/Table/table-participants/table-participants/table-participants.component.ts
@@ -1,34 +1,44 @@
-import { ParticipantsService } from './../../../../global/services/participants/participants.service';
-import { participant } from 'src/app/pages/components/utils/participant';
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { ParticipantsService } from "./../../../../global/services/participants/participants.service";
+import { participant } from "src/app/pages/components/utils/participant";
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
+import { SocketsService } from "src/app/global/services";
 
 @Component({
-  selector: 'table-participants',
-  templateUrl: './table-participants.component.html',
-  styleUrls: ['./table-participants.component.css']
+  selector: "table-participants",
+  templateUrl: "./table-participants.component.html",
+  styleUrls: ["./table-participants.component.css"],
 })
-
 export class TableParticipantsComponent implements OnInit {
   selectedName: string;
   participants: participant[];
   @Output() participantEvent = new EventEmitter<string>();
 
-  constructor(private participantsService: ParticipantsService) 
-  {}
+  constructor(
+    private participantsService: ParticipantsService,
+    private socketService: SocketsService
+  ) {}
 
   ngOnInit(): void {
-    this.participantsService.getAll().subscribe(data =>{
+    this.participantsService.getAll().subscribe((data) => {
       this.participants = data as participant[];
-    }
-    );        
+    });
+
+    this.socketService
+      .syncMessages("participants/change")
+      .subscribe((event) => {
+        this.loadParticipants();
+      });
   }
 
-  participantClicked(name: string){
+  participantClicked(name: string) {
     this.participantEvent.emit(name);
-    if(name == this.selectedName)
-      this.selectedName = undefined;
-    else
-      this.selectedName = name;
+    if (name == this.selectedName) this.selectedName = undefined;
+    else this.selectedName = name;
   }
 
+  loadParticipants() {
+    this.participantsService.getAll().subscribe((data) => {
+      this.participants = data as participant[];
+    });
+  }
 }
