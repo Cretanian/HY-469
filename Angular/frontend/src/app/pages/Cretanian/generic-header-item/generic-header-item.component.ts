9cf6b6bf4d0ce86a38022a038d6fcd23d530a03d
Cretanian <stelios.stamatakis.sp.9@gmail.com>
2020-12-15 14:16:10 +0200

    added somethings vol3. FINAL vol3 FINAL vol3 FINAL vol3 FINAL FINAL FINAL OSU!

--- Angular/frontend/src/app/pages/Cretanian/generic-header-item/generic-header-item.component.ts
+++ Angular/frontend/src/app/pages/Cretanian/generic-header-item/generic-header-item.component.ts
@@ -7,20 +7,21 @@ import { InOutDevicesService } from 'src/app/global/services/Cretanian/InOut-dev
   styleUrls: ['./generic-header-item.component.css']
 })
 export class GenericHeaderItemComponent implements OnInit {
-  volume_:volume;
+  ela:volume[];
   dropdownOptions: dropdownOption[] = [];
-
+  InVolume:number;
+  OutVolume:number;
   constructor(private inoutdevicesService: InOutDevicesService) { }
 
   ngOnInit(): void {
     this.inoutdevicesService.getAll("asd").subscribe(data => {
       this.dropdownOptions = data as dropdownOption[];
-      console.log(this.dropdownOptions);
     });
     
-    this.inoutdevicesService.getAll2("asd").subscribe(data => {
-      this.volume_ = data as volume;
-      console.log('eee' +this.volume_);
+    this.inoutdevicesService.getAll2("asd").subscribe(data2 => {
+      this.ela = data2 as volume[];
+      this.InVolume = this.ela[0].in_volume;
+      this.OutVolume = this.ela[0].out_volume;
     });
   }
 }
@@ -28,6 +29,10 @@ export class GenericHeaderItemComponent implements OnInit {
 class volume{
   in_volume:number;
   out_volume:number;
+  constructor(){
+    this.in_volume = 0;
+    this.out_volume = 0;
+  }
 }
 
 class dropdownOption{
