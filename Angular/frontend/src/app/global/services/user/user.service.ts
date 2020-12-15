669d9383cf40f17d2b03647a26ca35ef33ffe764
Antos2 <antosdapro@gmail.com>
2020-12-16 01:11:07 +0200

    Mute self added in table,tv pointer,phone good night everybodyy

--- Angular/frontend/src/app/global/services/user/user.service.ts
+++ Angular/frontend/src/app/global/services/user/user.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from "@angular/core";
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute } from "@angular/router";
 import { Observable } from "rxjs";
 import { TaskModel } from "../../models";
 import { HttpClient } from "@angular/common/http";
 import { map } from "rxjs/operators";
 import { environment } from "src/environments/environment";
 import * as _ from "lodash";
-import { IpService } from './ip.service'
+import { IpService } from "./ip.service";
 
 @Injectable({
   providedIn: "root",
@@ -15,41 +15,35 @@ export class UserService {
   private hostURL: string;
   private headers = { "content-type": "application/json" };
 
-  constructor(
-      private http: HttpClient,
-      private ipService: IpService
-  ){
-
+  constructor(private http: HttpClient, private ipService: IpService) {
     this.hostURL = environment.host;
   }
 
-  public setUserIP(username: string){
+  public setUserIP(username: string) {
     //find ip of client
     let ip: string;
-    this.ipService.getIPAddress().subscribe(
-        (res: any) => {
-            ip = res.ip;
-            return this.http.post(
-                `${this.hostURL}/api/user/setUserIP`,
-                {
-                    ip: ip,
-                    username: username
-                },
-                { headers: this.headers }
-            ).subscribe();
-        }
-    )
-
-    
+    this.ipService.getIPAddress().subscribe((res: any) => {
+      ip = res.ip;
+      return this.http
+        .post(
+          `${this.hostURL}/api/user/setUserIP`,
+          {
+            ip: ip,
+            username: username,
+          },
+          { headers: this.headers }
+        )
+        .subscribe();
+    });
   }
 
-  public getUserData(ip: number){
+  public getUserData(ip: number) {
     return this.http.post(
       `${this.hostURL}/api/user/getUserData`,
       {
-          ip: ip
+        ip: ip,
       },
-      { headers: this.headers }   
-    )
+      { headers: this.headers }
+    );
   }
 }
